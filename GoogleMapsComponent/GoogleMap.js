9000dc3af0f187d5579aa23038d17df2daefcde9
Paul Rene Nichols <paul.rene.nichols@gmail.com>
2016-07-05 15:47:02 -0500

    map-markers-0003
    * adding Marker class
    * this is a base class for Marker wrapper classes
    * GoogleMap now has a way to handle panning

--- GoogleMapsComponent/GoogleMap.js
+++ GoogleMapsComponent/GoogleMap.js
@@ -4,38 +4,110 @@ import React, { Component,
 import ReactDOM               from 'react-dom';
 import google                 from 'google';
 
+import _                      from 'lodash/core';
+
+import Marker                 from './Marker';
+
 
 export class GoogleMap extends Component {
-  static defaultProps = {
-    containerStyle: {
-      width: '100%',
-      height: '100%'
-    }
+  static propTypes = {
+    center:           PropTypes.shape({
+      lat: PropTypes.number,
+      lng: PropTypes.number
+    }),
+    containerStyle:   PropTypes.object,
+    mapOptions:       PropTypes.object,
+    markers:          PropTypes.arrayOf(PropTypes.oneOfType([
+      PropTypes.object,
+      PropTypes.instanceOf(Marker)
+    ])),
+    subscribePanTo:   PropTypes.func,
+    unsubscribePanTo: PropTypes.func,
+    zoom:             PropTypes.number
   };
 
-  static propTypes = {
-    containerStyle: PropTypes.object,
-    mapLoaded: PropTypes.func
+  static defaultProps = {
+    containerStyle: {
+      width:  '100%',
+      height: '100%'
+    },
+    mapOptions: {
+      center: {
+        lat:    30.3074625,
+        lng:    -98.0335911
+      },
+      mapTypeControl: false,
+      streetViewControl: false,
+      zoom:     10
+    },
+    markers:  [],
+    subscribePanTo: () => {},
+    unsubscribePanTo: () => {}
   };
 
   state = {
     googleMap: null
   };
 
+  _mapMarkers = {};
 
   constructor(props) {
     super(props);
   }
 
+  mapOptions() {
+    const { center, mapOptions, zoom } = this.props;
+
+    if (center) {
+      mapOptions.center = center;
+    }
+
+    if (zoom) {
+      mapOptions.zoom = zoom;
+    }
+
+    return mapOptions;
+  }
+
+  processMarkers(markers, map) {
+    this._mapMarkers = markers.reduce(function (reduction, marker) {
+      reduction[marker.id] = new google.maps.Marker({
+        label:    marker.label,
+        map:      map,
+        position: marker.position,
+        title:    marker.title,
+      });
+
+      return reduction;
+    }, {});
+  }
+
+  panTo = (msg, latLng) => {
+    this.state.googleMap.panTo(latLng);
+  }
+
   componentDidMount() {
-    const gmap = new google.maps.Map(ReactDOM.findDOMNode(this), {
-      center: {lat: 30.3074625, lng: -98.0335911},
-      zoom: 10
-    });
+    const { markers }  = this.props;
+
+    const gmap = new google.maps.Map(ReactDOM.findDOMNode(this), this.mapOptions());
 
     this.setState({
       googleMap: gmap
     });
+
+    this.processMarkers(markers, gmap);
+
+    this.props.subscribePanTo(this.panTo);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    this.processMarkers(nextProps.markers, this.state.googleMap);
+
+    this.state.googleMap.setOptions(this.mapOptions());
+  }
+
+  componentWillUnmount() {
+    this.props.unsubscribePanTo(this.panTo);
   }
 
   render() {
@@ -44,10 +116,6 @@ export class GoogleMap extends Component {
     return (
       <div style={containerStyle}>Map</div>
     );
-  }
-
-  getMap() {
-    return this.state.googleMap;
   }
 
 }
---
+++ GoogleMapsComponent/Marker.js
@@ -0,0 +1,16 @@
+export class Marker {
+  // get the marker icon
+  get icon()      {}
+
+  // get the id of the Marker
+  // must be unique for panning, animation
+  get id()        {}
+
+  get label()     {}
+
+  // get the position of the marker
+  get position()  {}
+
+  // get the title of the marker
+  get title()     {}
+}
