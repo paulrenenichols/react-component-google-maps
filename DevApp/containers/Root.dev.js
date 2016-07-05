981bbedf01c78ae81992d806f4bc5d6b6a81cf4f
Paul Rene Nichols <paul.rene.nichols@gmail.com>
2016-07-05 15:50:35 -0500

    map-markers-0006
    * adding application store to DevSandBox window object in dev mode

--- DevApp/containers/Root.dev.js
+++ DevApp/containers/Root.dev.js
@@ -14,6 +14,7 @@ window.DevSandBox.Perf                  = Perf;
 export default class Root extends Component {
   constructor(props) {
     super(props);
+    window.DevSandBox.store = this.props.store;
   }
 
   render() {
